2ec7a23378a5db003b7d6d1a176490662e78056a
JPTicse <jpticse@gmail.com>
2022-10-12 21:57:41 -0500

    Task 3

--- models/base_model.py
+++ models/base_model.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python3
 """ Module main base_model.py"""
 
-
 import uuid
+from datetime import datetime
+formato = "%Y-%m-%dT%H:%M:%S.%f"
+
 class BaseModel:
     def __init__(self):
-        self.id = uuid.uuid4()
-        self.created_at = ""
-        self.updated_at = ""
+        """Initialize data"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
-        return '[{}] ({}) {}'.format(self.__class__, self.id, self.__dict__)
+        """Function that returns str representation"""
+        return '[{}] ({}) {}'.format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        self.updated_at = ""
+        """Method to update updated_at with current datetime"""
+        self.updated_at = datetime.now()
 
     def to_dict(self):
-        return self.__dict__
+        """returns dictionary with all values"""
+        dict_temp = self.__dict__.copy()
+        dict_temp["__class__"] = self.__class__.__name__
+        dict_temp["created_at"] = self.created_at.isoformat()
+        dict_temp["updated_at"] = self.updated_at.isoformat()
+        return dict_temp
