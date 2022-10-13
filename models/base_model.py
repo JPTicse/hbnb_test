990a7adba74a3c2ad26edc1b90d48f481cfd7cf5
JPTicse <jpticse@gmail.com>
2022-10-12 22:14:54 -0500

    Casi task4

--- models/base_model.py
+++ models/base_model.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
 """ Module main base_model.py"""
 
+from curses import keyname
 import uuid
 from datetime import datetime
 formato = "%Y-%m-%dT%H:%M:%S.%f"
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize data"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if len(kwargs) > 0:
+            for key,value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, formato)
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
 
     def __str__(self):
         """Function that returns str representation"""
