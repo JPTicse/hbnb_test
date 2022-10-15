77dc45363df9dbe0625c0a9556b868b09c3d0310
JPTicse <jpticse@gmail.com>
2022-10-15 01:24:51 -0500

    task 7 advance all

--- console.py
+++ console.py
@@ -6,6 +6,7 @@ import cmd
 import sys
 
 from  models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -39,16 +40,72 @@ class HBNBCommand(cmd.Cmd):
             new_instance = BaseModel()
             new_instance.save()
             print(new_instance.id)
-    
-    def do_show(self, name_class, id):
-        """Prints the string representation of an instance based on the
-        class name and id. (Ex: $ show BaseModel 1234-1234-1234)"""
-        if not name_class:
-            print("**class name missing**")
-        elif name_class != 'BaseModel':
+            
+    def do_show(self, arg):
+        """Prints the string representation of an instance based
+        on the class name and id.
+        """
+        list_arg = arg.split(" ")
+
+        if len(arg) == 0:
+            print("** class name missing ** ")
+            return
+
+        elif list_arg[0] != 'BaseModel':
             print("** class doesn't exist **")
+            return
 
+        elif len(list_arg) < 2:
+            print("** instance id missing **")
+            return
 
+        else:
+            dict_all_obj = storage.all()
+            string = f'{list_arg[0]}.{list_arg[1]}'
 
+            if string not in dict_all_obj.keys():
+                print("** no instance found **")
+
+            else:
+                print(dict_all_obj[string])
+
+    def do_destroy(self, classDelId):
+        """Deletes an instance based on the class name and id 
+        (save the change into the JSON file). 
+        Ex: $ destroy BaseModel 1234-1234-1234"""
+
+        list_arg = classDelId.split(" ")
+
+        if not classDelId:
+            print("**class name missing**")
+            return
+
+        elif list_arg[0] != 'BaseModel':
+            print("** class doesn't exist **")
+            return
+
+        elif len(list_arg) < 2:
+            print("** instance id missing **")
+            return
+
+        else:
+            dict_all_obj = storage.all()
+            string = f'{list_arg[0]}.{list_arg[1]}'
+
+            if string not in dict_all_obj.keys():
+                print("** no instance found **")
+
+            else:
+                del(dict_all_obj[string])
+                storage.save()
+            
+    def do_all(self, newclass):
+        """Prints all string representation of all instances based or not on the
+        class name. Ex: $ all BaseModel or $ all.
+        * The printed result must be a list of strings
+        * If the class name doesn’t exist, print ** class doesn't exist **
+        (ex: $ all MyModel)"""
+
+        print(self)
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
